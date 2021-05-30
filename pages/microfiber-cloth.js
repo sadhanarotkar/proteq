18ecf8af4c77a7783b6454cbf9b4d963f8a9770c
Sadhana <sadhanarotkar@gmail.com>
2021-05-30 20:36:04 +0530

    test3 commit

--- pages/microfiber-cloth.js
+++ pages/microfiber-cloth.js
@@ -33,7 +33,6 @@ export default function MicrofiberCloth() {
               <div className="row justify-content-center align-items-center">
                 <div className="col-xl-5 col-lg-6 col-md-6 col-sm-12">
                   <h2 className="heading mb-md-4">Microfiber cloth</h2>
-                  <h2 className="heading mb-md-4">Microfiber cloth</h2>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer</p>
                 </div>
                 <div className="col-xl-5 col-lg-5 col-md-6 col-sm-10 text-center">
